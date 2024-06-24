90b5faacc30cc7917a1f5e7c813786d55bc6a38d
maria69 <maryamnz5@gmail.com>
2024-06-24 10:43:43 +0330

    first commit

---
+++ src/PackageServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Vendor\Package;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Register bindings or services
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Bootstrap any package services
+    }
+}
